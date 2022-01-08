b5aebfc491b0018c0c611670d6cd318807e815ca
Michael Latta <michael.latta@fast-forward-it.de>
2022-01-08 09:19:44 +0100

    new string method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Print me");
     }
+
+    public String getString() {
+        return "This is a string";
+    }
 }
